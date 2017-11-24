84720bc7271ce74a756a9ac07796129e86aa1611
martinkelly23 <martkelly92@gmail.com>
2017-11-24 14:28:51 +0000

    movimientos individuales

--- app.py
+++ app.py
@@ -36,6 +36,33 @@ def index():
 
     return render_template('form.html')
 
+@app.route('/movimientos', methods = ['GET', 'POST'])
+def mov():
+
+    # Comunicacion mediante puerto serie
+    # Comando para listar los puertos disponibles: python -m serial.tools.list_ports
+    # ser = serial.Serial('COM1')  # open serial port
+    # print(ser.name)         # check which port was really used
+    # ser.write(b'hello')     # write a string
+    # ser.close()             # close port
+
+    if request.method == 'POST':
+        data = request.form
+        frecuencia = data["frec"]
+        # Datos de la conexion a la BD
+        cnx = mysql.connector.connect(user='root', password='',
+                                  host='localhost',
+                                  database='estacion')
+        cursor = cnx.cursor()
+        query = ("UPDATE frecuencia_muestreo SET valor = %s WHERE id=1")
+        cursor.execute(query, (frecuencia,))
+        # Commit cambios en la BD
+        cnx.commit()
+        # Cierro la conexion
+        cnx.close()
+
+    return render_template('movimiento.html')
+
 
 if __name__ == "__main__":
     # Define HOST y PUERTO para accerder
